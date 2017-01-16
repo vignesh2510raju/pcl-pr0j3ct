29cd439b6443d02b9460b78d914c57ba0a5d730b
vignesh2510raju <vignesh2510raju@gmail.com>
2017-01-15 23:12:42 -0600

    first commit
    first commit

---
+++ build/CMakeFiles/cylinder_segmentation_new.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cylinder_segmentation_new.dir/src/cylinder_segmentation_new.cpp.o"
+  "cylinder_segmentation_new.pdb"
+  "cylinder_segmentation_new"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cylinder_segmentation_new.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/cylinder_segmentation_trial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cylinder_segmentation_trial.dir/src/cylinder_segmentation_trial.cpp.o"
+  "cylinder_segmentation_trial.pdb"
+  "cylinder_segmentation_trial"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cylinder_segmentation_trial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
