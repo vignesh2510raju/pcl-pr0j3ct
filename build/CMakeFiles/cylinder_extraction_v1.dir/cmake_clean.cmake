3b012cbf89cd12e8488eec6942b5236a3a546732
vignesh2510raju <vignesh2510raju@gmail.com>
2017-01-15 23:10:06 -0600

    first commit
    first commit

---
+++ build/CMakeFiles/cylinder_extraction_v1.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1_visualization.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1_user_input.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1_far_away_points.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1_cluster.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1_plane_from_cluster.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1_cylinder_segmentation.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/cylinder_extraction_v1_voxel_grid.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/read_transformations.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/read_matrices_pose.cpp.o"
+  "CMakeFiles/cylinder_extraction_v1.dir/src/read_velo_to_cam.cpp.o"
+  "cylinder_extraction_v1.pdb"
+  "cylinder_extraction_v1"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cylinder_extraction_v1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
